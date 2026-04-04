dbea65c1bbcd49e571257d71075d9a7bd4195ea3
David Barban <david@nabrab.com>
2026-04-03 21:19:50 -0700

    Refactor Set 4 Assignment 1 - replace subqueries with CTE

--- set04/set04_assignment1_employeesWithAboveAverageSalaray.sql
+++ set04/set04_assignment1_employeesWithAboveAverageSalaray.sql
@@ -1,34 +1,31 @@
 -- Set 4 Assignment 1
 -- Select employees with salary greaer than their department average
 
+WITH employee_info AS (
+    SELECT
+        e.name,
+        e.salary,
+        d.id AS department_id,
+        d.department_name
+    FROM employees AS e
+    JOIN employee_departments AS ed
+        ON e.id = ed.employee_id
+    JOIN departments AS d
+        ON ed.department_id = d.id
+),
+department_avg AS (
+    SELECT
+        department_id,
+        AVG(salary) AS average_salary
+    FROM employee_info
+    GROUP BY department_id
+)
 SELECT
-	ei.name AS employee_name,
-    ei.salary AS employee_salary,
-    ei.department_name
-FROM
-(
-  SELECT 
-      e.name,
-      e.salary,
-      d.department_name
-  FROM employees AS e
-  JOIN employee_departments AS ed
-      ON e.id = ed.employee_id
-  JOIN departments AS d
-      ON ed.department_id = d.id
-) AS ei
-JOIN
-(
-  SELECT 
-	d.department_name,
-    AVG(e.salary) AS average_salary
-  FROM employees AS e
-  JOIN employee_departments AS ed
-      ON ed.employee_id = e.id
-  JOIN departments AS d
-      ON ed.department_id = d.id
-  GROUP BY d.department_name
-) AS di
-ON ei.department_name = di.department_name
-WHERE ei.salary > di.average_salary
+    employee_info.name AS employee_name,
+    employee_info.salary AS employee_salary,
+    employee_info.department_name
+FROM employee_info
+JOIN department_avg
+    ON employee_info.department_id = department_avg.department_id
+WHERE employee_info.salary > department_avg.average_salary;
 	
